4f4dce07f1c8c6204e4ae534c87507b3c2ff0b18
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-11-05 08:48:52 +0200

    test

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const fs = require("fs");
-const Tour = require("../models/tourModel");
-const User = require("../models/userModel");
-const Review = require("../models/reviewModel");
+const Tour = require("../../models/tourModel");
+const User = require("../../models/userModel");
+// const Review = require("../models/reviewModel");
 // console.log(app.get("env"));
 
 dotenv.config({ path: "../../config.env" });
