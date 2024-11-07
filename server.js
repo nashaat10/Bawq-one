d9c42982742bf6937f12c6bb05465e87e7d0fdec
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-11-07 12:38:02 +0200

    add unhandledRejection

--- server.js
+++ server.js
@@ -5,16 +5,20 @@ const mongoose = require("mongoose");
 
 const DB = process.env.DATABASE_URL;
 
-mongoose
-  .connect(DB)
-  .then(() => {
-    console.log("DB connection successful");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect(DB).then(() => {
+  console.log("DB connection successful");
+});
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
+});
+
+// handel unhandledRejection like database connection
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  //server.close() give the server time to finish all the request that are still being handled then the server killed
+  server.close(() => {
+    process.exit(1);
+  });
 });
