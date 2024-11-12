eca4e6111d041a7589c410c7944b49ef2d98f110
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-11-12 08:01:46 +0200

    up

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/AppError");
-const { log } = require("console");
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
