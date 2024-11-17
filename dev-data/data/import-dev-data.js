8232b6e6a7de1bd85de7df2667c3cf9d2ea7a9a7
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-11-17 15:57:11 +0200

    add dev data to database

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const fs = require("fs");
 const Tour = require("../../models/tourModel");
-// const User = require("../../models/userModel");
-// const Review = require("../models/reviewModel");
+const User = require("../../models/userModel");
+const Review = require("../../models/reviewModel");
 // console.log(app.get("env"));
 
 dotenv.config({ path: "./config.env" });
@@ -15,14 +15,17 @@ mongoose.connect(DB).then(() => {
   console.log("DB connection successful");
 });
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
-// const reviews = JSON.parse(fs.readFileSync("../data/reviews.json", "utf-8"));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
+);
 
 const importData = async () => {
   try {
     await Tour.create(tours, { validateBeforeSave: false });
-    // await User.create(users, { validateBeforeSave: false });
-    // await Review.create(reviews);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log("Data loaded successfully");
     process.exit(1);
   } catch (err) {
@@ -34,8 +37,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
-    // await User.deleteMany();
-    // await Review.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data deleted successfully");
     process.exit(1);
   } catch (err) {
