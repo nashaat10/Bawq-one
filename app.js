cbf69e8865dc8c008567d1cb4693c86bd3f7a27a
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-11-17 08:01:23 +0200

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const tourRouter = require("./routes/tourRoutes");
 const morgan = require("morgan");
 const AppError = require("./utils/AppError");
 const globalErrorHandler = require("./controllers/errorController");
+const reviewRouter = require("./routes/reviewRoutes");
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -14,6 +15,7 @@ app.use(express.static("./public"));
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 // used to handel all routes
 app.all("*", (req, res, next) => {
