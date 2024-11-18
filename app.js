230b970bce5f9fde7b1b26edd225e5b48461adb8
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-11-18 15:34:27 +0200

    -mupdate

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const userRouter = require("./routes/userRoutes");
 const tourRouter = require("./routes/tourRoutes");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 const AppError = require("./utils/AppError");
 const globalErrorHandler = require("./controllers/errorController");
 const reviewRouter = require("./routes/reviewRoutes");
@@ -10,8 +11,16 @@ const reviewRouter = require("./routes/reviewRoutes");
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP ,Please try again later",
+});
 app.use(express.json());
 app.use(express.static("./public"));
+// apply limiter to all api that starts with /api
+app.use("/api", limiter);
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
