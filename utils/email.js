6c5c6c6a5514a941f9d2fe4c1547f613bad73b1c
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-12-04 15:39:47 +0200

    up

--- utils/email.js
+++ utils/email.js
@@ -1,15 +1,37 @@
 const nodemailer = require("nodemailer");
 
+module.exports = class Email {
+  constructor(user, url) {
+    (this.to = user.email),
+      (this.firstName = user.name.split(" ")[0]),
+      (this.url = url),
+      (this.from = `Mohamed Nashaat ${process.env.EMAIL_FROM}`);
+  }
+  createTransport() {
+    if (process.env.NODE_ENV === "production") {
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+  send(template, subject) {
+    const mailOptions = {
+      from: "Mohamed Nashaat",
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
+  }
+};
+
 const sendEmail = async (options) => {
   // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
   // 2) Define the email options
 
   const mailOptions = {
@@ -21,5 +43,3 @@ const sendEmail = async (options) => {
   // 3) Actually send the email
   await transporter.sendMail(mailOptions);
 };
-
-module.exports = sendEmail;
